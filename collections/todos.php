7bb22a91964f04253b18f93d93fb119d003b62e9
Harshit Mayur Shah <hs574@afsconnect2.njit.edu>
2017-12-10 01:54:29 -0500

    added todos files

--- collections/todos.php
+++ collections/todos.php
@@ -2,6 +2,7 @@
 class todos extends database\collection
 {
     protected static $modelName = 'todo';
+    //write.
     //This is the function to write to find tasks by user ID for listing on their homepage.
     //Don't forget to return the record set see findAll in the collection class
     public static  function findTasksbyID($userid) {}
