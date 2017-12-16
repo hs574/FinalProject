68370e915e442e787882d8510d02766029e82fae
Harshit Mayur Shah <hs574@afsconnect2.njit.edu>
2017-12-16 01:52:00 -0500

    recent process request file

--- core/http/processRequest.php
+++ core/http/processRequest.php
@@ -1,6 +1,5 @@
 <?php
 namespace http;
-//by using the use here you don't have to put http on each class in that namespace
 class processRequest
 {
     //this is the main function of the program to calculate the response to a get or post request
@@ -14,9 +13,12 @@ class processRequest
         //this determines the method to call for the controller
         $controller_method = $requested_route->method;
         //these echo helps figure out the controller name and method
-        // echo $controller_name . '</br>';
-        // echo $controller_method . '</br>';
+        //echo $controller_name . '</br>';
+        //echo $controller_method . '</br>';
         //I use a static for the controller because it doesn't have any properties
+        //echo "name ".$controller_name;
+        echo '<br>';
+        //echo "method ".$controller_method;
         $controller_name::$controller_method();
     }
     //this function matches the request to the correct controller
@@ -27,19 +29,22 @@ class processRequest
         $page = request::getPage();
         $action = request::getAction();
         //these are helpful for figuring out the action and method being requested
-        //echo 'Action: ' . $action . '</br>';
-        //echo 'Page: ' . $page . '</br>';
-        //echo 'Request Method: ' . $request_method . '</br>';
+        echo 'Action: ' . $action . '</br>';
+        echo 'Page: ' . $page . '</br>';
+        echo 'Request Method: ' . $request_method . '</br>';
         //this gets the routes objects, you need to add routes to add pages and follow the template of the route specified
         $routes = \routes::getRoutes();
+        //print_r($routes);
         $foundRoute = NULL;
         //this figures out which route matches the page being requested in the URL and returns it so that the controller and method can be called
         foreach ($routes as $route) {
+          echo $route->action;
             if ($route->page == $page && $route->http_method == $request_method && $route->action == $action) {
                 $foundRoute = $route;
                 break;
             }
         }
+        //print_r($foundRoute);
         if (is_null($foundRoute)) {
             controller::getTemplate('notfound');
             exit;
