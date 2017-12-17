7ff749ed5d33f4a3a5ab3babab4ca5745edc58bb
Harshit Mayur Shah <hs574@afsconnect2.njit.edu>
2017-12-17 02:02:43 -0500

    recent  file

--- collections/todos.php
+++ collections/todos.php
@@ -6,10 +6,15 @@ class todos extends database\collection
     //Don't forget to return the record set see findAll in the collection class
     public static  function findTasksbyID($userid) {
         $tableName = get_called_class();
-        $sql = 'SELECT * FROM ' . $tableName . ' WHERE ownerid ="' . $userid.'"';
-        //echo $sql;
+        $sql = 'SELECT * FROM ' . $tableName . ' WHERE ownerid = ?';
         //grab the only record for find one and return as an object
-        $recordsSet = self::getResults($sql);
-        return $recordsSet;
+        $recordsSet = self::getResults($sql, $userid);
+        if (is_null($recordsSet)) {
+            return FALSE;
+        } else {
+            return $recordsSet;
+        }
+    
     }
 }
+?>
