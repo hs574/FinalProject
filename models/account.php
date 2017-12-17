55896248cdcfceee8b06e6a2c487d8285387957a
Harshit Mayur Shah <hs574@afsconnect2.njit.edu>
2017-12-17 02:19:13 -0500

    recent  file

--- models/account.php
+++ models/account.php
@@ -24,18 +24,22 @@ final class account extends \database\model
         return $records;
     }
     //add a method to compare the passwords this is where bcrypt should be done and it should return TRUE / FALSE for login
-    public function setPassword($password) {
+    public static function setPassword($password) {
         $password = password_hash($password, PASSWORD_DEFAULT);
         return $password;
     }
-    public function checkPassword($password) {
-        $checkpsw = password_verify($password, $this->password);
-        print_r($checkpsw);
-        return $checkpsw;
+    public function checkPassword($LoginPassword) {
+        return password_verify($LoginPassword, $this->password);
     }
+    /*public function validate()
+    {
+        $valid = TRUE;
+        echo 'myemail: ' . $this->email;
+        if($this->email == '') {
+            $valid = FALSE;
+            echo 'nothing in email';
+        }
+        return $valid;
+    }*/
 }
 ?>
-
-
-
-
